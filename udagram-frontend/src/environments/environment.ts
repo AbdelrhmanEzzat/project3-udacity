d8d0c7612d4508b3a38e87166d10271150477cd6
AbdelrhmanEzzat <abdoezzat222@gmail.com>
2022-10-10 20:04:19 +0200

    00

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a4bc4189ed1cb42d1adede74a447a80a-223296819.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
